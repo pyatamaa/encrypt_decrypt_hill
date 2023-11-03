219fb39b63e56df3099978572398800026ca51fe
Nur Pratama <92738041+pyatamaa@users.noreply.github.com>
2023-11-03 12:40:43 +0700

    Update hill_cipher.php
    mengubah koneksi database kedalam file khusus koneksi

--- hill_cipher.php
+++ hill_cipher.php
@@ -1,16 +1,6 @@
 <?php
 require('functions.php');
-// Koneksi ke Database
-$db_host = 'localhost';
-$db_user = 'root';
-$db_pass = '';
-$db_name = 'hill_cipher';
-
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
-if (!$conn) {
-    die("Koneksi ke database gagal: " . mysqli_connect_error());
-}
+require('koneksi.php');
 
 // Simpan Hasil ke Database
 $result = "";
